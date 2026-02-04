123d51535a5cee49e70c19ceffdaab01721ad850
lloydieeeeeeee-2025 <lloyddeleon@thelewiscollege.edu.ph>
2026-02-05 01:03:24 +0800

    update db host

--- frontend/library/tlcchatmatedb/route.js
+++ frontend/library/tlcchatmatedb/route.js
@@ -1,14 +1,14 @@
 import mysql from "mysql2/promise"
 
 export const chatmate = mysql.createPool({
-    host: process.env.DB_HOST || "172.19.0.1",
+    host: process.env.DB_HOST || "db",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "kmlV3@VVVE$$56nnmh",
     database: process.env.DB_NAME || "tlcchatmate",
     port: Number(process.env.DB_PORT) || 3306,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
 });
 
 
